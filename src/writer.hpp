010ac1976fb950d56100922b5c2a327b9da826d9
Carlos Rafael Giani <dv@pseudoterminal.org>
2013-03-23 17:57:54 +0100

    Added comments to writer header
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

--- src/writer.hpp
+++ src/writer.hpp
@@ -38,59 +38,81 @@ namespace eglinfo
 {
 
 
+/**
+ * Abstract class for writing EGL & GLES & OpenVG information.
+ * It uses a state-based model similar to SAX. API processing code takes in a reference to a writer,
+ * and calls these functions. How the information is conveyed to the user is up to the writer.
+ */
 class writer
 {
 public:
+	// These begin and end the overall writing. Writers can start and end the output or document here.
 	virtual void begin_write() = 0;
 	virtual void end_write() = 0;
 
+	// Begin and end an information block about the specific API.
 	virtual void begin_api(EGLenum const p_api, char const *p_api_name) = 0;
 	virtual void end_api() = 0;
+	// This is used to signify a space between APIs. Writers can use this to put in spacings between API
+	// information blocks.
 	virtual void next_api() = 0;
 
+	// Begin and end information about EGL itself.
 	virtual void begin_egl_info() = 0;
 	virtual void end_egl_info() = 0;
+	// Write main EGL information.
 	virtual void write_main_egl_info(
 		  EGLint const p_major_version, EGLint const p_minor_version
-		, char const *p_vendor
-		, char const *p_version
-		, char const *p_client_APIs
-		, char const *p_extensions
+		, char const *p_vendor         // vendor string
+		, char const *p_version        // version string
+		, char const *p_client_APIs    // whitespace-separated list of supported APIs
+		, char const *p_extensions     // whitespace-separated list of extensions
 	) = 0;
+	// Begin and end the list of EGL configurations.
 	virtual void begin_write_egl_configs(EGLint const p_num_configs) = 0;
-	virtual void write_egl_config(egl_config const &p_egl_config) = 0;
 	virtual void end_write_egl_configs() = 0;
+	// Write an EGL configuration.
+	virtual void write_egl_config(egl_config const &p_egl_config) = 0;
+	// Used if there are no EGL configurations (instead of an empty EGL configs block).
 	virtual void write_no_egl_configs() = 0;
 
+	// Common function for writing main information about a GL API (OpenGL ES 1.x, OpenGL ES 2.x, and Desktop OpenGL).
 	virtual void write_main_glapi_info(
-		  EGLenum const p_api
-		, char const *p_api_name
-		, char const *p_version
-		, char const *p_renderer
-		, char const *p_extensions
+		  EGLenum const p_api        // the EGL API name
+		, char const *p_api_name     // human-readable name of the API
+		, char const *p_version      // version string
+		, char const *p_renderer     // renderer string
+		, char const *p_extensions   // whitespace-separated list of extensions
 	) = 0;
 
+	// Common function for writing stats about a GL API (OpenGL ES 1.x, OpenGL ES 2.x, and Desktop OpenGL).
+	// Stats are information like: maximum number of varyings, maximum anisotropy, list of supported shader binary languages etc.
 	virtual void write_glapi_stats(
-		  EGLenum const p_api
-		, char const *p_api_name
-		, glapi_stats const &p_stats
+		  EGLenum const p_api         // the EGL API name
+		, char const *p_api_name      // human-readable name of the API
+		, glapi_stats const &p_stats  // main and shader specific stats about the API
 	) = 0;
 
+	// Write main information about OpenVG.
 	virtual void write_main_vg_info(
-		  char const *p_vendor
-		, char const *p_version
-		, char const *p_renderer
-		, char const *p_extensions
+		  char const *p_vendor         // vendor string
+		, char const *p_version        // version string
+		, char const *p_renderer       // renderer string
+		, char const *p_extensions     // whitespace-separated list of extensions
 	) = 0;
 
+	// Begin and end the list of image formats and whether or not they are hardware-accelerated.
 	virtual void begin_write_vg_image_format_acceleration() = 0;
-	virtual void write_vg_image_format_acceleration(
-		  int const p_format
-		, char const *p_name
-		, bool const p_accelerated
-	) = 0;
 	virtual void end_write_vg_image_format_acceleration() = 0;
-	
+	// Write information about a specific format.
+	virtual void write_vg_image_format_acceleration(
+		  int const p_format          // the OpenVG format name
+		, char const *p_name          // human-readable version of the name
+		, bool const p_accelerated    // true = format is hardware-accelerated  false = otherwise
+	) = 0;
+
+	// Write information about path datatype acceleration
+	// true = datatype is hardware-accelerated  false = otherwise
 	virtual void write_vg_path_datatype_acceleration(
 		  bool const p_signed8
 		, bool const p_signed16
